91b40729ab5583ea609e8f66464d7d1d39f50298
Usen <iliasovusenbek@gmail.com>
2024-02-25 22:48:20 +0100

    shop-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpg' ,
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo' ,
+          category: 'chairs' ,
+          price: '49.99',
+        },
+        {
+          id: 2,
+          title: 'Диван коричневый',
+          img: 'sofa-brown.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi rem ullam qui dolorem facilis recusandae cupiditate eligendi voluptatum explicabo',
+          category: 'sofas',
+          price: '199.99',
+          },
+          {
+          id: 3,
+          title: 'Стол журнальный',
+          img: 'table-wood.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum maiores non consequatur voluptates a recusandae, placeat modi.',
+          category: 'tables',
+          price: '79.99',
+          },
+          {
+          id: 4,
+          title: 'Письменный стол',
+          img: 'desk.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio autem impedit quidem a rerum, repellat architecto atque cupiditate voluptatum',
+          category: 'desks',
+          price: '29.99',
+          },
+          {
+          id: 5,
+          title: 'Шкаф белый',
+          img: 'cabinet-white.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui autem porro debitis ea ex modi vero amet corporis nemo.',
+          category: 'cabinets',
+          price: '149.99',
+          },
+          {
+          id: 6,
+          title: 'Журнальный стол',
+          img: 'desk-brown.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quisquam facilis ab, suscipit tempora ratione ipsam labore odio, a reprehenderit',
+          category: 'sofas',
+          price: '219.99',
+          }      
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header orders={this.state.orders}/>
+      <Items items={this.state.items} onAdd={this.addToOrder} />
+      <Footer/>
     </div>
   );
+}
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el =>  {
+      if(el.id === item.id)
+      isInArray=true;
+    })
+    if(!isInArray)
+    this.setState({orders: [...this.state.orders, item]}, 
+    )
+  }
 }
 
 export default App;
